96093d4b3c2eedaf1b8185f0e42cffa806a052eb
Pitin Vladimir <vladimir@pitin.su>
2013-04-12 17:18:25 +1100

    bug fixed

---
+++ db/migrate/001_change_user_preferences.rb
@@ -0,0 +1,8 @@
+class ChangeUserPreferences < ActiveRecord::Migration
+  def up 
+    add_column :user_preferences, :favourite_project_id, :integer
+  end
+  def down
+    remove_column :user_preferences, :favourite_project_id
+  end
+end
