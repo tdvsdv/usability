0d62754ef28e2f9f99491ea65b3b4ecaf71e27d3
tdvsdv <vladimir@pitin.su>
2013-06-03 17:41:54 +1100

    Closed issue #9884

--- init.rb
+++ init.rb
@@ -5,12 +5,20 @@ Redmine::Plugin.register :usability do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  settings :default => {:custom_help_url => ''},
+           :partial => 'settings/usability'
+
+  delete_menu_item :top_menu, :help
 end
 
 Rails.application.config.to_prepare do
   ApplicationHelper.send(:include, Usability::ApplicationHelperPatch)
   Redmine::MenuManager::MenuHelper.send(:include, Usability::MenuManagerPatch)
   User.send(:include, Usability::UserPatch)
+  Redmine::Info.send(:include, Usability::InfoPatch::Redmine::Info)
 end
 
 require 'usability/view_hooks'
+
+Redmine::Plugin.find('usability').menu :top_menu, :help,Usability::InfoPatch::Redmine::Info.help_url, :last => true
