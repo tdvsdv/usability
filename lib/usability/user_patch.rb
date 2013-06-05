f9aeaf4d493bfabdd99a6768ad9a3afa249aad52
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2013-06-05 13:12:12 +1100

    Bind with favorite_project. Method rename

--- lib/usability/user_patch.rb
+++ lib/usability/user_patch.rb
@@ -5,6 +5,7 @@ module Usability
     base.send(:include, InstanceMethods)  
 
     base.class_eval do
+      belongs_to :favourite_project, :class_name => "Project", :foreign_key => :favourite_project_id
 
     end
     
@@ -15,7 +16,7 @@ module Usability
   end
   
   module InstanceMethods
-    def favourite_project
+    def get_favourite_project
       Project.select("#{Project.table_name}.*, COUNT(#{Journal.table_name}.id) AS count").joins({:issues => :journals}).where("#{Journal.table_name}.user_id = ?", id).group("#{Project.table_name}.id").order("count DESC").first
     end
   end
