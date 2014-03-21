5849fee3a015c09be994c1ea2fc108b748327fb8
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-03-21 15:36:07 +1100

    Fixed top_menu resize on ajax_counters loaded

--- assets/javascripts/usability.js
+++ assets/javascripts/usability.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
 /* ----- from a_small_things starts ---- */
-  $('.contextual').next('div[style*="clear: both"]').remove().end().prev('div[style*="clear: both"]').remove()
+  $('.contextual').next('div[style*="clear: both"]').remove().end().prev('div[style*="clear: both"]').remove();
 
-  var contextual = $('#update').prev('.contextual')
-  if(contextual.length>0) {
-    $("<div/>", {"class": "H"}).insertBefore('#history').append($('#issue-changesets'), $('#history'));
-    $("<div/>", {"class": "H"}).insertBefore('#update').append(contextual);
+  var contextual = $('#update').prev('.contextual');
+  if (contextual.length > 0) {
+    $('<div/>', {'class': 'H'}).insertBefore('#history').append($('#issue-changesets'), $('#history'));
+    $('<div/>', {'class': 'H'}).insertBefore('#update').append(contextual);
   }
 
-  $('#add_filter_select, #available_columns, #group_by, #query_sort_criteria_attribute_0, #query_sort_criteria_attribute_1, #query_sort_criteria_attribute_2').each(function() {
-    var selected = $(this).children('option:selected') //Only for firefox
-    $(this).children('option').sort(function(a, b){
+  $('#add_filter_select, #available_columns, #group_by, #query_sort_criteria_attribute_0, #query_sort_criteria_attribute_1, #query_sort_criteria_attribute_2').each(function () {
+    var selected = $(this).children('option:selected'); //Only for firefox
+    $(this).children('option').sort(function (a, b) {
             if ($(a).text() == 'NA')
                 {
                 return 1;
@@ -21,21 +21,21 @@ $(document).ready(function(){
                 }
             return ($(a).text().toLowerCase() > $(b).text().toLowerCase()) ? 1 : -1;
           }).appendTo($(this));
-    selected.attr("selected", "selected")  //Only for firefox
+    selected.attr('selected', 'selected')  //Only for firefox
   });
 
-  $(document.body).on("click", "input.issue_due_date", function(){
+  $(document.body).on('click', 'input.issue_due_date', function () {
       $(this).data('set_auto_val', false);
-      $(this).removeClass("highlight");
+      $(this).removeClass('highlight');
     });
 
-  $('td.project>a').each(function(index){
+  $('td.project>a').each(function (index) {
     $(this).parent().html($(this).html());
   });
 
   $('#ajax-indicator').remove();
 
-  $('#sidebar').children('a[href$="issues?set_filter=1"]').each(function(index){
+  $('#sidebar').children('a[href$="issues?set_filter=1"]').each(function (index) {
     $(this).prev('h3').remove();
     $(this).remove();
   });
@@ -43,16 +43,16 @@ $(document).ready(function(){
   $('#sidebar').children('a[href$="gantt"]').remove();
   $('#sidebar').children('a[href$="report"]').next().remove();
 
-  $("#attributes select[name*=assigned_to_id] option:contains(' мне ')").each(function(){
+  $('#attributes select[name*=assigned_to_id] option:contains(" мне ")').each(function () {
     $(this).remove();
   });
 
 
-  $(document.body).on('click', 'form[data-remote="true"] input[type=submit], a.icon-del[data-remote="true"], a.show_loader[data-remote="true"]', function(){
+  $(document.body).on('click', 'form[data-remote="true"] input[type=submit], a.icon-del[data-remote="true"], a.show_loader[data-remote="true"]', function () {
     jQuery(document.body).data('ajax_emmiter', jQuery(this))
   });
 
-  $(document).ajaxStart(function() {
+  $(document).ajaxStart(function () {
     // alert('ajax started')
     obj = jQuery(document.body).data('ajax_emmiter')
     if(typeof obj != 'undefined') {
@@ -63,7 +63,7 @@ $(document).ready(function(){
     jQuery(document.body).data('ajax_emmiter', undefined)
   });
 
-  $(document).ajaxStop(function() {
+  $(document).ajaxStop(function () {
     jQuery("div.loader:empty").remove();
     jQuery('.ajax_hidden_emmiter').show();
   });
@@ -74,17 +74,16 @@ $(document).ready(function(){
   var close_sidebar = $('<a/>', { href: '#',
                                   id: 'close_sidebar_icon',
                                   class: 'R close_sidebar icon',
-                                  click: function() {
-                                    if($(this).hasClass('close_sidebar')) {
+                                  click: function () {
+                                    if ($(this).hasClass('close_sidebar')) {
                                       hide_sidebar($(this))
-                                    }
-                                    else {
+                                    } else {
                                       show_sidebar($(this))
                                     }
                                   }
                       });
   $('#sidebar').prepend(close_sidebar);
-  $('#sidebar').dblclick(function() {
+  $('#sidebar').dblclick(function () {
     hide_sidebar($('#close_sidebar_icon'));
   });
 
@@ -93,7 +92,7 @@ $(document).ready(function(){
     showTab(loc[1])
   }
 
-  $('.tabs a').click(function(){
+  $('.tabs a').click(function () {
     var url = $(this).attr('href').split('?tab=');
     var loc = location.href.split('#');
     location.href = loc[0] + '#' + url[1];
@@ -102,71 +101,71 @@ $(document).ready(function(){
 
   var user_preferences = []
   user_preferences['top_menu_event'] = 'mouseclick';
-  $('#usability_user_preferences div').each(function(){
+  $('#usability_user_preferences div').each(function () {
     user_preferences[$(this).attr('class')] = $(this).html();
   });
 
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     $('.sub_menu').popover('hide');
     $('.us_dropdown_menu').hide();
   });
 
-  $(document).click(function(event) {
-      if($(event.target).parents('div.popover').length == 0)
+  $(document).click(function (event) {
+      if ($(event.target).parents('div.popover').length == 0)
         $('.sub_menu').popover('hide');
-      if($(event.target).parents('.us_dropdown_menu').length == 0)
+      if ($(event.target).parents('.us_dropdown_menu').length == 0)
         $('.us_dropdown_menu').hide();
   });
 
   if (user_preferences['top_menu_event'] == 'mouseclick') {
-    $('.sub_menu').bind('click', function(){$('.sub_menu').not($(this)).popover('hide')});
+    $('.sub_menu').bind('click', function () { $('.sub_menu').not($(this)).popover('hide') });
     $('.sub_menu').popover({ html: "true",
                              trigger: "click",
                              placement: "bottom",
                              container: 'body',
-                             content: function(){return $('.'+$(this).attr('data-content-class')).map(function() {return $(this).html()}).get().join('')}
+                             content: function () { return $('.'+$(this).attr('data-content-class')).map(function () { return $(this).html(); }).get().join('') }
     });
 
-    $('.sub_menu, a.my-name').click(function(){return false;});
+    $('.sub_menu, a.my-name').click(function () { return false });
   }
 
   if (user_preferences['top_menu_event'] == 'mouseover') {
     $('.sub_menu')
-    .popover({ html: "true",
-               trigger: "manual",
-               placement: "bottom",
+    .popover({ html: 'true',
+               trigger: 'manual',
+               placement: 'bottom',
                container: 'body',
-               content: function(){return $('.'+$(this).attr('data-content-class')).map(function() {return $(this).html()}).get().join('')}
+               content: function () {return $('.'+$(this).attr('data-content-class')).map(function () {return $(this).html()}).get().join('')}
     })
-    .mouseenter(function(e) {
+    .mouseenter(function (e) {
        var t = $(this);
-       window.setTimeout(function() {
-                           if(typeof $(document).data('popover_enter') == 'undefined' || !$(document).data('popover_enter')) {
+       window.setTimeout(function () {
+                           if (typeof $(document).data('popover_enter') == 'undefined' || !$(document).data('popover_enter')) {
                              t.popover('show');
                              $('.sub_menu').not(t).popover('hide')
                            }
                          }, 100);
     })
-    .mouseleave(function(e) {
+    .mouseleave(function (e) {
       var t = $(this)
-      window.setTimeout(function() {
-                             if(typeof $(document).data('popover_enter') == 'undefined' || !$(document).data('popover_enter'))
+      window.setTimeout(function () {
+                             if (typeof $(document).data('popover_enter') == 'undefined' || !$(document).data('popover_enter'))
                                  t.popover('hide')
                          }, 100);
     });
-    $(document.body).on("mouseenter", 'div.popover', function(){$(document).data('popover_enter', true)});
-    $(document.body).on("mouseleave", 'div.popover', function(){$(document).data('popover_enter', false), $(this).hide()});
+    $(document.body).on('mouseenter', 'div.popover', function () { $(document).data('popover_enter', true) });
+    $(document.body).on('mouseleave', 'div.popover', function () { $(document).data('popover_enter', false), $(this).hide() });
   }
 
 
   $('.splitcontentleft ul').each(function() {
-    if($(this).children().length==0)
+    if ($(this).children().length==0)
       $(this).remove();
   });
 
 
   $('a.icon, #main-menu li a, #top-menu li a:not(:has(span))').each(function(index){
-    if($(this).html() == $(this).text())
+    if ($(this).html() == $(this).text())
       $(this).html('<span>'+$(this).html()+'</span>');
   });
 
@@ -190,36 +189,27 @@ $(document).ready(function(){
   });
 
   $('#q').blur(function(index) {
-    if (typeof $("#q").data('last_width') != 'undefined') {
-      $(this).animate({ width: $("#q").data('last_width') }, 300, 'swing', rebuild_menu);
+    if (typeof $('#q').data('last_width') != 'undefined') {
+      $(this).animate({ width: $('#q').data('last_width') }, 300, 'swing', rebuild_menu);
     }
   });
 
-  $("#account .logout").parent().remove();
-  $('<li class="menu_expander"><a href="#" class="icon icon-expand I">&nbsp;</a></li>').appendTo("#top-menu>ul");
+  $('#account .logout').parent().remove();
+  $('<li class="menu_expander"><a href="#" class="icon icon-expand I">&nbsp;</a></li>').appendTo('#top-menu>ul');
   $('<ul class="I us_dropdown_menu" id="dropdown_top_menu"></ul>').appendTo(document.body);
   rebuild_menu();
 
-  $(window).resize(function() {
-    $('.sub_menu').popover('hide');
-    shrink_q();
-    if( $('#q').is(':focus') ) {
-      expand_q_if_possible();
-    }
-
-    var top_width = $("#top-menu").width();
-    var dd_menu_width = $("#top-menu .menu_expander").outerWidth();
-
-    if(top_width > $("#account").outerWidth()+$("#quick-search").outerWidth()+24+dd_menu_width) {
-      $("#quick-search").show();
-    } else {
-      $("#quick-search").hide();
-    }
-    rebuild_menu();
+  $(window).resize(function () {
+    usability_reallocate_top_menu();
   });
 
-  $(".icon-expand").click(function() {
-    var dropdown = $("#dropdown_top_menu");
+  // for ajax_counters plugin
+  $(document.body).on('counters_refreshed', function () {
+    usability_reallocate_top_menu();
+  });
+
+  $('.icon-expand').click(function() {
+    var dropdown = $('#dropdown_top_menu');
     if (dropdown.is(':visible')) {
       dropdown.hide();
     } else {
@@ -230,18 +220,37 @@ $(document).ready(function(){
 
 });
 
-function shrink_q() {
-  if (typeof $("#q").data('last_width') != 'undefined') {
-    $("#q").width($("#q").data('last_width'));
+function usability_reallocate_top_menu () {
+  $('.sub_menu').popover('hide');
+  shrink_q();
+
+  if ($('#q').is(':focus')) {
+    expand_q_if_possible();
+  }
+
+  var top_width = $('#top-menu').width();
+  var dd_menu_width = $('#top-menu .menu_expander').outerWidth();
+
+  if (top_width > $('#account').outerWidth()+$('#quick-search').outerWidth()+24+dd_menu_width) {
+    $('#quick-search').show();
+  } else {
+    $('#quick-search').hide();
+  }
+
+  rebuild_menu();
+}
+
+function shrink_q () {
+  if (typeof $('#q').data('last_width') != 'undefined') {
+    $('#q').width($('#q').data('last_width'));
   }
 }
 
-function expand_q_if_possible() {
-  var ul_width = $("#top-menu>ul").width();
-  // var dd_width = $("#top-menu>ul .menu_expander:first").width();
+function expand_q_if_possible () {
+  var ul_width = $('#top-menu>ul').width();
   var li_width = 0;
-  var q = $("#q");
-  $("#top-menu>ul>li").each(function(index) {
+  var q = $('#q');
+  $('#top-menu>ul>li').each(function (index) {
     li_width += $(this).outerWidth();
   });
   if (ul_width-li_width > 250-q.outerWidth()) {
@@ -252,13 +261,13 @@ function expand_q_if_possible() {
 
 
 function rebuild_menu () {
-  var ul_width = $("#top-menu>ul").width(); //inner width
-  var dd_width = $("#top-menu>ul .menu_expander:first").width();
+  var ul_width = $('#top-menu>ul').width(); //inner width
+  var dd_width = $('#top-menu>ul .menu_expander:first').width();
   var li_width = dd_width;
-  $("#dropdown_top_menu").hide();
-  $("#dropdown_top_menu>li").addClass('I');
-  $("#dropdown_top_menu>li").insertBefore("#top-menu .menu_expander");
-  $("#top-menu>ul>li").each(function(index) {
+  $('#dropdown_top_menu').hide();
+  $('#dropdown_top_menu>li').addClass('I');
+  $('#dropdown_top_menu>li').insertBefore('#top-menu .menu_expander');
+  $('#top-menu>ul>li').each(function (index) {
     if (!$(this).hasClass('menu_expander')) {
       li_width += $(this).outerWidth();
       if (ul_width < li_width) {
@@ -267,7 +276,7 @@ function rebuild_menu () {
       $(this).removeClass('I');
     }
   });
-  if ($("#dropdown_top_menu>li").length > 0) {
+  if ($('#dropdown_top_menu>li').length > 0) {
     $('.menu_expander').removeClass('I');
   } else {
     $('.menu_expander').addClass('I');
@@ -286,27 +295,26 @@ function show_dropdown (dropdown, obj) {
 }
 /* ----- from a_small_things starts ---- */
 
-function hide_sidebar(t)
-  {
-    $('#sidebar').hide()
-    t.addClass('show_sidebar')
-    t.removeClass('close_sidebar')
-    $('#content').data('margin-right', $('#content').css('margin-right'))
-    $('#content').css('margin-right', '16px')
-    $('#sidebar').before(t)
-  }
-function show_sidebar(t)
-  {
-    $('#sidebar').show()
-    t.removeClass('show_sidebar')
-    t.addClass('close_sidebar')
-    $('#content').css('margin-right',  $('#content').data('margin-right'))
-    $('#sidebar').prepend(t)
-  }
+function hide_sidebar (t) {
+    $('#sidebar').hide();
+    t.addClass('show_sidebar');
+    t.removeClass('close_sidebar');
+    $('#content').data('margin-right', $('#content').css('margin-right'));
+    $('#content').css('margin-right', '16px');
+    $('#sidebar').before(t);
+}
+
+function show_sidebar (t) {
+    $('#sidebar').show();
+    t.removeClass('show_sidebar');
+    t.addClass('close_sidebar');
+    $('#content').css('margin-right',  $('#content').data('margin-right'));
+    $('#sidebar').prepend(t);
+}
 
 // walk-around for non-working rails confirm in some cases (like for a.show_loader[data-remote="true"] - return false does not stop event)
-function hard_confirm(label,event) {
-  if(!confirm(label)){
+function hard_confirm (label,event) {
+  if (!confirm(label)) {
     event.stopPropagation();
     return false;
   }
@@ -314,51 +322,47 @@ function hard_confirm(label,event) {
 }
 
 
-function checkDueDateCalcIsHandy()
-  {
-    var start_date=$('input.issue_start_date');
-    var due_date=$('input.issue_due_date');
-    var estimated_hours=$('input.issue_estimated_hours');
-    if(start_date && estimated_hours && due_date ) {
-      if(due_date.val()=='' || due_date.data('set_auto_val') ) {
-        if(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val()) && /^[0-9\.]{1,}$/.test(estimated_hours.val()))
-          calcDueDate(start_date, due_date, estimated_hours);
-      }
-      else {
-        if(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val())
-          && /^[0-9\.]{1,}$/.test(estimated_hours.val())
-          && /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(due_date.val())) {
-          var date_arr=start_date.val().split(".");
-          var issue_start_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));
-          date_arr=due_date.val().split(".");
-          var issue_due_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));
-          var days=(estimated_hours.val()/8).ceil();
-          if(issue_due_date.getDate()-issue_start_date.getDate()<days-1)
-            calcDueDate(start_date, due_date, estimated_hours);
+function checkDueDateCalcIsHandy () {
+    var start_date = $('input.issue_start_date');
+    var due_date = $('input.issue_due_date');
+    var estimated_hours = $('input.issue_estimated_hours');
+    if (start_date && estimated_hours && due_date) {
+        if (due_date.val() == '' || due_date.data('set_auto_val')) {
+            if (/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val()) && /^[0-9\.]{1,}$/.test(estimated_hours.val()))
+                calcDueDate(start_date, due_date, estimated_hours);
+        } else {
+            if (/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val())
+               && /^[0-9\.]{1,}$/.test(estimated_hours.val())
+               && /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(due_date.val())) {
+                var date_arr = start_date.val().split(".");
+                var issue_start_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));
+                date_arr = due_date.val().split(".");
+                var issue_due_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));
+                var days = (estimated_hours.val()/8).ceil();
+                if (issue_due_date.getDate()-issue_start_date.getDate()<days-1)
+                    calcDueDate(start_date, due_date, estimated_hours);
+            }
         }
-      }
     }
-  }
+}
 
 function calcDueDate(start_date, due_date, estimated_hours) {
-  var date_arr=start_date.val().split(".");
+  var date_arr = start_date.val().split(".");
   var issue_due_date = new Date();
   var issue_start_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));
-  var days=(estimated_hours.val()/8).ceil();
+  var days = (estimated_hours.val()/8).ceil();
   var issue_due_date = new Date(issue_start_date.getTime()+(days-1)*1000*24*60*60+1);
   //issue_due_date.setDate(issue_start_date.getDate()+days-1);
 
-  var month= issue_due_date.getMonth()+1;
-  month=(month<10)? '0'+month : month;
-  var day=issue_due_date.getDate();
-  day=(day<10)? '0'+day: day;
+  var month = issue_due_date.getMonth() + 1;
+  month = (month<10) ? '0'+month : month;
+  var day = issue_due_date.getDate();
+  day = (day<10) ? '0'+day : day;
   var year = issue_due_date.getFullYear();
   due_date.val(day+"."+month+"."+year);
   due_date.data('set_auto_val', true);
   due_date.addClass('highlight');
 }
 
-setInterval(function() {
-    checkDueDateCalcIsHandy();
-  }, 1000)
+setInterval(function () { checkDueDateCalcIsHandy(); }, 1000)
 /* ----- from a_small_things ends ---- */
