f0fc44a50549828cbc5c55d5d6054d04b87df68d
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-04-03 18:47:54 +1100

    #18978 done. Custom error implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,7 @@ get 'users/:id/edit_usability_preferences', :controller => 'users', :action => '
 put 'users/:id/update_usability_preferences', :controller => 'users', :action => 'update_usability_preferences', :id => /\d+/
 
 get 'users/:id/show_user_details', :controller => 'users', :action => 'show_user_details', :id => /\d+/, :as => "show_user_details"
+
+match '404', :to => 'us_errors#error_404'
+match '500', :to => 'us_errors#error_500'
+# get '422', :to => 'us_errors#error_422'
