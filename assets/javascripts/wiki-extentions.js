a6b6972bb84292429985ddd5a82f83c55e001e6d
Alexey Glukhov <gluhov1985@gmail.com>
2014-06-16 16:55:01 +1100

    #19948 Added button.

--- assets/javascripts/wiki-extentions.js
+++ assets/javascripts/wiki-extentions.js
@@ -8,3 +8,30 @@ jsToolBar.prototype.elements.cut = {
     }
   }
 }
+
+jsToolBar.prototype.draw_button = function(button_before, name, mode){
+  this.setMode(mode);
+  var b, tool, newTool;
+  b = this.elements[name];
+
+  var disabled =
+      b.type == undefined || b.type == ''
+      || (b.disabled != undefined && b.disabled)
+      || (b.context != undefined && b.context != null && b.context != this.context);
+
+  if (!disabled && typeof this[b.type] == 'function') {
+    tool = this[b.type](name);
+    if (tool) newTool = tool.draw();
+    if (newTool) {
+      this.toolNodes[name] = newTool;
+      this.toolbar.insertBefore(newTool, button_before);
+    }
+  }
+};
+
+$(document).ready(function(){
+  var cut = $('.jstb_cut').clone(true, true);
+  $('.jstb_cut').remove();
+  var code_button = document.getElementById('space1');
+  wikiToolbar.draw_button(code_button, 'cut');
+});
