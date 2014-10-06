0cc603ca1151564acc0b45ab7298711a8d53504e
Alexey Glukhov <gluhov1985@gmail.com>
2014-10-06 11:25:47 +1100

    added localStorage check

--- assets/javascripts/usability.js
+++ assets/javascripts/usability.js
@@ -266,7 +266,8 @@ $(document).ready(function () {
                                       }
                           });
       $('#sidebar').prepend(close_sidebar);
-      var closed = localStorage["sidebar_closed"] || false;
+
+      var closed = ('localStorage' in window && window['localStorage'] !== null) ? localStorage["sidebar_closed"] : false;
       if (closed === "true") {
         hide_sidebar($('#close_sidebar_icon'));
       }
