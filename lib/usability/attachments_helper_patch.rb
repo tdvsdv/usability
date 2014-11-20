df3bfbdbbc24b3237058c5eb1bc951467cb9afd2
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-11-20 19:53:57 +1000

    #25035 support other display of attachments

--- lib/usability/attachments_helper_patch.rb
+++ lib/usability/attachments_helper_patch.rb
@@ -13,10 +13,15 @@ module Usability
 
       def link_to_attachments_with_usability(container, options = {})
         default = link_to_attachments_without_usability(container, options)
-        if ((Setting.plugin_usability || { })['enable_download_attachments_all_in_one'])
-          if (container.attachments != [ ])
-            default << '<br>'.html_safe
-            default << link_to(l(:label_usability_download_all_in_one), { controller: :attachments, action: :download_all, id: container.attachments[0].id }, class: 'icon icon-us-download-all in_link').html_safe
+        if ((Setting.plugin_usability || {})['enable_download_attachments_all_in_one'])
+          if container.attachments != []
+            link_text = link_to(l(:label_usability_download_all_in_one), { controller: :attachments, action: :download_all, id: container.attachments[0].id }, class: 'icon icon-us-download-all in_link').html_safe
+            if default.index('<!-- all_attaches -->')
+              default = default.gsub('<!-- all_attaches -->', link_text).html_safe
+            else
+              default << '<br>'.html_safe
+              default << link_text
+            end
           end
         end
 
