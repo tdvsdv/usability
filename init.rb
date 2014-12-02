f659d1cc575e90b7adcc2a6da722a1145bfca76f
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-12-02 19:57:39 +1000

    #25906 js variant

--- init.rb
+++ init.rb
@@ -35,7 +35,6 @@ end
 
 Rails.application.config.to_prepare do
   ApplicationHelper.send(:include, Usability::ApplicationHelperPatch)
-  AccountController.send(:include, Usability::AccountControllerPatch)
   User.send(:include, Usability::UserPatch)
   UsersController.send(:include, Usability::UsersControllerPatch)
   WelcomeController.send(:include, Usability::WelcomeControllerPatch)
